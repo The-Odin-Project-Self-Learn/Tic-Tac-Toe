d57e65110bca5815f6bd21fb110cf11b6f7cc80a
Mihir <mihirsingh@berkeley.edu>
2025-02-05 17:38:13 -0800

    implemented some more gameBoard methods

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 const gameBoard = (() => {
     const board = []; 
-    const rows = 3;
-    const columns = 3;
-     /*
+    const cells = 9;
+
+    //populate the board with empty space
+    /*
     [
         [''], [''], ['']
         [''], [''], [''],
         [''], [''], ['']
     ] 
     */
-
-    //populate the board with empty space
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
+    const resetBoard = () => {
+        for (let i = 0; i < cells; i++) {
+            board[i] = [];
             board[i].push('');
         }
     }
-
+    
     //make the board state accessible outside of the factory
     const getBoard = () => board;
 
-});
+    //checks the value in the cell positioned at the given index
+    const checkCell = (index) => {
+        if (index < 0 | index >= board.length) {
+            return 'non-existent cell';
+        }
+        
+        if (board[index].includes("X")) {
+            return 'X';
+        }
+
+        if (board[index].includes("O")) {
+            return 'O';
+        }
+    }
+
+    //update cell with a particular marker at the given index
+    const updateCell = (index, marker) => {
+        
+    } 
+
+
+})();
 
 
 
